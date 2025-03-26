ea31a86f9a33e7a5aa605cc74e3812644ecdbcef
Dongseup Shim <108253922+ShimDongseup@users.noreply.github.com>
2025-03-26 15:50:01 +0900

    Update 멀리 뛰기.js

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.js
@@ -1,6 +1,8 @@
+// 피보나치 수열 문제
 // 효진이가 n번째 칸에 도달하는 경우의 수
 // n-1번째 칸까지 오는 방법 + n-2번째 칸까지 오는 방법의 합
 // dp[n] = dp[n-1] + dp[n-2]
+
 function solution(n) {
   const dp = new Array(n + 1).fill(0);
   dp[1] = 1;
